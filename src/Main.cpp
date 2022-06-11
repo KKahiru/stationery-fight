7dcf15c7931c78f4266972956f295489813ace87
KKahiru <kkahiru@icloud.com>
2022-06-11 17:37:19 +0900

    Add summon button
    Integrate header files.
    Add summon button.
    Change window size.
    Replace some code with Siv3D ones.
    Embed original svg files.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,13 +1,129 @@
 # include <Siv3D.hpp>
 #include <iostream>
-#include <vector>
 #include "json.hpp"
-#include "Main.hpp"
 using namespace std;
-using namespace stfi;
 using json = nlohmann::json;
 
+const int texture_size = 120;
+//jsonファイルを開いてパーズする変数
+inline json open_json_file(String file_path){
+    //jsonファイルを開く
+    TextReader json_file{ file_path };
+    //jsonファイルを開けたか判定
+    if (json_file.isOpen()){
+        cout << "Opened "<< file_path <<"." << endl;
+    } else{
+        throw range_error("Failed to open \'" + file_path.narrow() + "\'.");
+    }
+    const string json_law = json_file.readAll().narrow();
+    //jsonファイルの中身を読み込んでパーズする
+    cout << json_file.readAll().narrow() << endl;
+    json json_data = json::parse(json_law);
+    return json_data;
+}
+//コンフィグ
+const json config = open_json_file(U"resource/config.json");
+//コンフィグの値を取得する関数
+template <class Type>
+Type get_config_value(string type, string json_path){
+    json result = config.at(json::json_pointer("/ability_config/" + type + "/" + json_path)).get<Type>();
+    return result;
+}
+//game_unitのクラス
+class game_unit{
+public:
+    //味方かのbool値
+    bool is_friend;
+    //game_unitのタイプ
+    string type;
+    //攻撃力
+    uint16 attack_power;
+    //移動速度
+    uint16 speed;
+    //攻撃時にセットするクールダウン
+    uint16 reset_cooldown;
+    //攻撃範囲の始まり
+    uint16 attack_range_begin;
+    //攻撃範囲の終わり
+    uint16 attack_range_end;
+    //x座標
+    uint16 x;
+    //耐久値
+    int16 durability;
+    //クールダウン
+    uint16 cooldown = 0;
+    //コンストラクター
+    game_unit(string type, bool is_player_camp);
+    //通常時テクスチャー
+    Texture texture;
+    //攻撃時テクスチャー
+    Texture attack_texture;
+    //移動
+    void go();
+    //相手を攻撃
+    void attack(game_unit *target);
+};
+//コンストラクターの定義
+game_unit::game_unit(string type, bool is_friend){
+    if (is_friend){
+        this->x = 40;
+    } else{
+        this->x = Scene::Size().x - 40;
+    }
+    this->type = type;
+    this->is_friend = is_friend;
+    this->durability = get_config_value<uint16>(type, "durability");
+    this->attack_power = get_config_value<uint16>(type, "attack_power");
+    this->speed = get_config_value<uint16>(type, "speed");
+    this->reset_cooldown = get_config_value<uint16>(type, "cooldown");
+    if (is_friend){
+        this->texture = Texture{ Resource(U"resource/texture/" + Unicode::Widen(type) + U"_friend.png"), TextureDesc::Mipped };
+        this->attack_texture = Texture{ Resource(U"resource/texture/" + Unicode::Widen(type) + U"_friend_attack.png"), TextureDesc::Mipped };
+    } else{
+        this->texture = Texture{ Resource(U"resource/texture/" + Unicode::Widen(type) + U"_enemy.png"), TextureDesc::Mipped };
+        this->attack_texture = Texture{ Resource(U"resource/texture/" + Unicode::Widen(type) + U"_enemy_attack.png"), TextureDesc::Mipped };
+    }
+    this->attack_range_begin = get_config_value<uint16>(type, "attack_range_begin");
+    this->attack_range_end = get_config_value<uint16>(type, "attack_range_end");
+    
+}
+//移動する関数の定義
+void game_unit::go(){
+    //味方かの判定
+    if (this->is_friend){
+        //正の方向に移動
+        this->x += this->speed;
+    } else{
+        //負の方向に移動
+        this->x -= this->speed;
+    }
+}
+//相手を攻撃する関数の定義
+void game_unit::attack(game_unit *target){
+    //攻撃処理
+    target->durability -= this->attack_power;
+    //クールダウン
+    this->cooldown = this->reset_cooldown;
+}
+
+struct summon_button{
+    summon_button(string type,uint16 count);
+    string type;
+    Texture texture;
+    uint16 x;
+    Rect background;
+    
+};
+summon_button::summon_button(string type,uint16 count){
+    this->type = type;
+    this->x = texture_size * (count + 0.5);
+    this->texture = Texture{ Resource(U"resource/texture/" + Unicode::Widen(type) + U"_friend.png"), TextureDesc::Mipped };
+    this->background = Rect{texture_size * count,Scene::Size().y - texture_size,texture_size,texture_size};
+}
+
 void Main(){
+    //ウィンドウサイズの設定
+    Window::Resize(1067,600);
     //背景を白くする
     Scene::SetBackground(Palette::White);
     //攻撃音
@@ -15,48 +131,62 @@ void Main(){
     // 蓄積された時間（秒）
     double accumulator = 0.0;
     //ステージ上のgame_unitのリスト
-    vector<game_unit> game_units;
+    Array<game_unit> game_units;
+    //召喚ボタンのリスト
+    Array<summon_button> summon_buttons;
     //城を造る
     game_units.push_back(game_unit("castle",true));
     game_units.push_back(game_unit("castle",false));
+    //召喚ボタンの登録
+    for( int i=0; auto& el : config.at("all_types")) {
+        summon_buttons.push_back(summon_button(el.get<string>(),i));
+        i++;
+    };
+    //毎フレームごとの処理
     while (System::Update()) {
         //蓄積された秒数の記録
         accumulator += Scene::DeltaTime();
-        //game_unitを召喚する仮GUI
-        if (SimpleGUI::Button(U"味方鉛筆召喚", Vec2{ 0, 100 }))
-        {
-            game_units.push_back(game_unit("pencil",true));
-        }
-        if (SimpleGUI::Button(U"敵鉛筆召喚", Vec2{ 200, 100 }))
-        {
-            game_units.push_back(game_unit("pencil",false));
-        }
-        if (SimpleGUI::Button(U"味方消しゴム召喚", Vec2{ 400, 100 }))
-        {
-            game_units.push_back(game_unit("eraser",true));
-        }
-        if (SimpleGUI::Button(U"敵消しゴム召喚", Vec2{ 600, 100 }))
-        {
-            game_units.push_back(game_unit("eraser",false));
-        }
         //game_unitの描画
         for (unsigned long int i = 0; i < game_units.size(); i++) {
             //自分の前に重なっているgame_unitのカウント数
             int overlapped_unit_count = 0;
-            for (long int i2 = 0; i2 < distance(game_units.begin(), game_units.begin() + i); i2++){
-                //カウント
-                overlapped_unit_count++;
+            for (long int j = 0; j < distance(game_units.begin(), game_units.begin() + i); j++){
+                if (game_units[j].x < game_units[i].x + texture_size/4*3 and game_units[j].x > game_units[i].x - texture_size/4*3 and game_units[j].type != "castle") {
+                    //カウント
+                    overlapped_unit_count++;
+                }
             }
             //攻撃しているか
             if (game_units[i].cooldown > game_units[i].reset_cooldown / 5 * 3){
                 //攻撃テクスチャーで描画
-                game_units[i].attack_texture.drawAt(game_units[i].x , Scene::Center().x - overlapped_unit_count);
+                game_units[i].attack_texture.resized(texture_size).drawAt(game_units[i].x , Scene::Center().y - overlapped_unit_count * texture_size / 8);
             } else {
                 //通常テクスチャーで描画
-                game_units[i].texture.drawAt(game_units[i].x , Scene::Center().x - overlapped_unit_count);
+                game_units[i].texture.resized(texture_size).drawAt(game_units[i].x , Scene::Center().y - overlapped_unit_count * texture_size / 8);
             }
         }
-        //game_unitの行動処理
+        //敵を召喚する簡易ボタン
+        if (SimpleGUI::Button(U"敵鉛筆召喚", Vec2{ 0, 100 }))
+        {
+            game_units.push_back(game_unit("pencil",false));
+        }
+        if (SimpleGUI::Button(U"敵消しゴム召喚", Vec2{ 150, 100 }))
+        {
+            game_units.push_back(game_unit("eraser",false));
+        }
+        if (SimpleGUI::Button(U"敵三角定規召喚", Vec2{ 350, 100 }))
+        {
+            game_units.push_back(game_unit("triangle",false));
+        }
+        //召喚ボタンの描画
+        for (unsigned long int i = 0; i<summon_buttons.size(); i++){
+            summon_buttons[i].background.draw(summon_buttons[i].background.mouseOver() ? ColorF{ 0.5, 0.5, 0.5 } : ColorF{ 0.6, 0.6, 0.6 }).drawFrame(4, 0, ColorF{ 0.1, 0.1, 0.1 });
+            summon_buttons[i].texture.resized(texture_size).drawAt(summon_buttons[i].x,Scene::Size().y-texture_size/2);
+            if (summon_buttons[i].background.leftClicked()) {
+                game_units.push_back(game_unit(summon_buttons[i].type,true));
+            }
+        }
+        //game_unitの行動処理（0.1秒刻み）
         while ( 0.1 <= accumulator) {
             for (unsigned long int i = 0; i < game_units.size(); i++) {
                 //情報を残すコード
@@ -79,6 +209,7 @@ void Main(){
                             }
                             //攻撃したフラグを立てる
                             has_attacked = true;
+                            break;
                         }
                     }
                     //攻撃したかの判定
