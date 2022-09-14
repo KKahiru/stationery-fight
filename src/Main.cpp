3722076d9e6bcf35fbda7cc8cb57031822b064d0
KKahiru <kkahiru@icloud.com>
2022-09-14 19:51:01 +0900

    Remade tutorial images
    Remade tutorial images.
    Changed window settings.

--- src/Main.cpp
+++ src/Main.cpp
@@ -869,7 +869,10 @@ public:
 
 
 void Main(){
-    ConfigJson = JSON::Load(Resource(U"resource/config.json"));
+    //ウィンドウサイズの設定
+    Window::Resize(960,540);
+    //ウィンドウタイトルの設定
+    Window::SetTitle(U"Stationery Fight!");
     // シーンマネージャーを作成
     App manager;
     // シーンの登録
@@ -877,12 +880,15 @@ void Main(){
     manager.add<GameScene>(U"GameScene");
     manager.add<RuleExplanation>(U"RuleExplanation");
     manager.add<DifficultySetting>(U"DifficultySetting");
-    //ウィンドウサイズの設定
-    Window::Resize(960,540);
     Scene::SetResizeMode(ResizeMode::Keep);
     Window::SetStyle(WindowStyle::Sizable);
     //オーディオアセットの登録
     AudioAsset::Register(U"choose", Resource(U"resource/sound/choose.ogg"));
+    //コンフィグのロード
+    ConfigJson = JSON::Load(Resource(U"resource/config.json"));
+    if (not ConfigJson) {
+        throw Error{U"Error! Failed to load a config file."};
+    }
     //毎フレームごとの処理
     while (System::Update()) {
         if (not manager.update()){
